451887b9c088c8f93767459f85448e3e02b3e36a
RAIMUNDO CALIGA CARDOSO <caliga.ba@gmail.com>
2023-10-03 18:35:55 -0300

    Finalizações

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -2,18 +2,28 @@ import styles from './Banner.module.css';
 import Titulo from '../Titulo'
 import { Link } from 'react-router-dom';
 import Botao from '../Botao';
+import VideoCard from '../VideoCard';
 
-function Banner({imagem}) {
+
+function Banner({ imagem }) {
     return (
-        <section className={styles.banner} style={{backgroundImage: `url('./image/banner-${imagem}.jpg')`}}>
+        <section className={styles.banner} style={{ backgroundImage: `url('./image/banner-${imagem}.jpg')` }}>
+
+
             <div className={styles.botao}>
                 <Link className={styles.link} to='./novovideo'>
                     <Botao texto="Novo video" />
                 </Link>
             </div>
-            <div className={styles.titulo}>
-                <Titulo titulo="Cube Rubik" texto="Exercite a memória, resolvendo o Cubo Mágico!"/>
+
+            <div className={styles.caixaBanner}>
+                <div className={styles.titulo}>
+                    <Titulo titulo="Cube Rubik" texto="Exercite a memória, resolvendo o Cubo Mágico!" />
+                </div>
+
+                <Link to="https://www.youtube.com/watch?v=0iePBCRcs24&t=3s" target='_blank'><VideoCard videoCard="https://i.ytimg.com/vi/0iePBCRcs24/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDgMf51CxSJSzzTssanaiowtg6Tfg" /></Link>
             </div>
+
         </section>
     )
 }
--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -2,17 +2,18 @@ import VideoCard from '../VideoCard';
 import styles from './Carousel.module.css';
 import videos from '../../json/db.json';
 import TituloCategoria from '../TituloCategoria';
+import { Link } from 'react-router-dom';
 // import setaDireita from './seta-direita.png';
 // import setaEsquerda from './seta-esquerda.png'
 
-function Carousel({titulo, texto}) {
+function Carousel({ titulo, texto }) {
 
-    const filtros = videos.filter( video => video.categoria === titulo );
+    const filtros = videos.filter(video => video.categoria === titulo);
     console.log(filtros);
 
 
     return (
-        
+
         <section className={styles.container}>
             {/* <img className={styles.setaDireita} src={setaDireita} alt='Seta a Direita' />
             <img className={styles.setaEsquerda} src={setaEsquerda} alt='Seta a Esquerda' /> */}
@@ -20,13 +21,13 @@ function Carousel({titulo, texto}) {
             <TituloCategoria titulo={titulo} texto={texto} />
 
             <div className={styles.galleryContainer}>
-                
+
                 <div className={styles.gallery}>
 
                     {filtros.map((video) => {
-                        
-                        return <VideoCard videoCard={video.urlImagem} key={video.id} />
-                        
+
+                        return <Link to={video.urlVideo} target='_blank'><VideoCard videoCard={video.urlImagem} key={video.id} /></Link>
+
                     })}
                 </div>
             </div>
